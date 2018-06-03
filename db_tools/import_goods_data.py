e423eed95bd35dbd0f3add15ba425cfaecbb2b58
Your Name <you@example.com>
2018-06-03 22:42:01 +0800

    zsj-20180603 merge be and fe

--- db_tools/import_goods_data.py
+++ db_tools/import_goods_data.py
@@ -5,7 +5,8 @@ import os
 
 
 pwd = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(pwd+"../")
+#sys.path.append(pwd+"../")
+sys.path.append("/home/zhongshijun/xshop_be")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MxShop.settings")
 
 import django
